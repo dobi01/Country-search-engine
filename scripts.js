9ff575c178f3723836a0913bf7d1f9325c8f8dea
Dobi Okrasa <dobi.okrasa@gmail.com>
2017-12-23 16:08:29 +0100

    Add styles & update scripts

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var url = 'https://restcountries.eu/rest/v2/name/',
+    countriesList = $('#countries'),
+    capitalsList = $('#capitals');
+
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+  if(!countryName.length) countryName = 'Poland';
+
+  $.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+  });
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+  capitalsList.empty();
+  resp.forEach(function(item){
+   	$('<li>').text(item.name).appendTo(countriesList);
+    $('<li>').text(item.capital).appendTo(capitalsList);
+  });
+}
+
+$('#country-name').keyup(searchCountries);
